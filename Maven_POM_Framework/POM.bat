7376c1d2581c86e8ce8b0245515cc94fdcdfe11b
Anil Yadav Avula <anil04503@gmail.com>
2022-07-19 19:27:16 +0530

    Bat file update

--- Maven_POM_Framework/POM.bat
+++ Maven_POM_Framework/POM.bat
@@ -12,7 +12,8 @@ echo 	*														 *
 echo 	******************************************************************************************************************
 @echo on
 cd C:\Automation_Testing\Maven_POM_Framework
-START /W mvn clean install
+mvn clean install
+
 
 
 
